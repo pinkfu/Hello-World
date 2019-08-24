d003117d8a4c1d948e8d1613ec8b57eee3c9f5c1
Kayla <lil.momma8707@gmail.com>
2019-08-23 20:17:37 -0500

    First commit for HelloWorld

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+Kayla Jordan
+CTEC 295-101
+Getting Started with Git
+*/
+package helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!!");      
+    }
+    
+}
